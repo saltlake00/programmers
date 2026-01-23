ae86a3a0a2dfe8f9470c16c5a4f7f1ced58a5518
saltlake00 <saltlake5470@gmail.com>
2026-01-23 10:59:38 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    int n = 0;
+    for(int i =0; i<seoul.size(); i++)
+    {
+        if(seoul[i]=="Kim")
+        {
+            n = i;
+            break;
+        }
+    }
+    
+    answer = "김서방은 " + std::to_string(n) + "에 있다";
+        
+    return answer;
+}

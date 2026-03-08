3b575dc5889d55ed95f91a036402d23bcb923bfd
saltlake00 <saltlake5470@gmail.com>
2026-03-08 17:16:38 +0900

    [level 1] Title: 시저 암호, Time: 0.06 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == ' ')
+        {
+            continue;
+        }
+        
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s[i] = (s[i]-'A' + n) % 26 + 'A';
+        }
+        
+        else if (s[i] >= 'a' && s[i] <= 'z') 
+        {
+            s[i] = (s[i] - 'a' + n) % 26 + 'a';
+        }
+        
+    }
+    
+    return s;
+}
